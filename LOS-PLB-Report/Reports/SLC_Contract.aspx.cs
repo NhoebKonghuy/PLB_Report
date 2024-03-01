9f9ecc484150bfcbcaf3137264258677a11844eb
Meng Keang <keang.meng@finig.asia>
2024-03-01 09:59:01 +0700

    [ADD FILE] SLC Contract

---
+++ LOS-PLB-Report/Reports/SLC_Contract.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LOS_PLB_Report.Reports
+{
+    public partial class SLC_Contract : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
